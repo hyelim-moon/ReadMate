4bfef1d922d8b54aac80c0be1b6d4f0058d7c950
문혜림 <mhl0612@kyungmin.ac.kr>
2025-04-10 18:45:52 +0900

    Initial commit

---
+++ src/main/java/SR/SR_Test/SrTestApplication.java
@@ -0,0 +1,13 @@
+package SR.SR_Test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrTestApplication.class, args);
+	}
+
+}
