371c255b44b93d43d5cdb7af5e251dea0e1ef0b8
dus <yeondy017@kyungmin.ac.kr>
2025-04-28 14:13:56 +0900

    다연 독서 경쟁 페이지 연결

--- src/main/frontend/src/components/Navbar.js
+++ src/main/frontend/src/components/Navbar.js
@@ -5,7 +5,7 @@ function Navbar() {
     return (
         <nav className={styles.navbar}>
             <Link to="/record"><button>독서 기록</button></Link>
-            <button>독서 경쟁</button>
+            <Link to="/competition"><button>독서 경쟁</button></Link>
             <Link to="/pointShop"><button>포인트샵</button></Link>
             <Link to="/community"><button>커뮤니티</button></Link>
         </nav>
